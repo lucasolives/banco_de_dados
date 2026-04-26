8b0c7912af1c8fb758ac2fc2f59d7a272b98166c
Lucascintra08 <lucascintra08@gmail.com>
2026-04-26 18:13:42 -0300

    Create 5. Criar Intervenção.sql

---
+++ scripts/views/5. Criar Intervenção.sql
@@ -0,0 +1,20 @@
+SELECT
+	e.matricula,
+	e.nome,
+	STRING_AGG(c.nome, ', ') AS curso,
+	COALESCE(STRING_AGG(g.titulo, ', '), 'Sem grupo de intervenção') AS grupos,
+	e.data_ingresso AS ingresso,
+	ed.semestre || '/' || c.total_semestres AS qtd_semestres,
+	ed.turmas
+FROM estatisticas_desempenho ed
+	INNER JOIN estudante e ON ed.id_estudante = e.id
+	LEFT JOIN curso c ON e.id_curso = c.id
+	LEFT JOIN estudante_grupo eg ON e.id = eg.id_estudante
+	LEFT JOIN grupo g ON eg.id_grupo = g.id
+GROUP BY
+	e.matricula,
+	e.nome,
+	e.data_ingresso,
+	ed.semestre,
+	c.total_semestres,
+	ed.turmas;
