4e3e9cacd32a11a58ef308063904d32b8fe6e36a
GabrielCastro75 <gabrielcastro.ec@gmail.com>
2026-05-04 14:29:28 -0300

    item 7: add all screens SQL

--- scripts/views/7_consulta_grupos.sql
+++ scripts/views/7_consulta_grupos.sql
@@ -1,7 +1,11 @@
+----------
+-- Lista Grupos
+----------
+
 SELECT
     g.titulo,
     g.semestre,
-    COUNT(DISTINCT eg.id_estudante) AS estudantes,
+    COUNT(DISTINCT mg.id_matricula) AS estudantes,
     TO_CHAR(g.data_criacao, 'DD/MM/YYYY') AS criacao,
     g.observacao,
     -- Lógica simples: Se houver curso, concatena ' de ' + lista. Se não, apenas o perfil.
@@ -16,7 +20,7 @@ SELECT
 FROM grupo g
     INNER JOIN usuario u ON g.id_usuario = u.id
     INNER JOIN perfil p ON u.id_perfil = p.id
-    LEFT JOIN estudante_grupo eg ON g.id = eg.id_grupo
+    LEFT JOIN matricula_grupo mg ON g.id = mg.id_grupo
 GROUP BY 
     g.id, 
 	g.titulo, 
@@ -26,3 +30,44 @@ GROUP BY
 	u.id, 
 	p.nome, 
 	g.ativo;
+    
+---------
+-- Registro de nova intervenção
+---------
+
+-- Nome e matrícula dos estudantes do Grupo selecionado
+SELECT
+    e.nome,
+    m.matricula
+FROM public.matricula_grupo mg
+    INNER JOIN public.matricula m ON mg.id_matricula = m.id
+    INNER JOIN public.estudante e ON m.id_estudante = e.id
+WHERE mg.id_grupo = 1;
+
+-- Criação da intervenção
+INSERT INTO public.intervencao (id, id_usuario, id_disciplina, id_curso, semestre, forma, formato, interacao, assunto, tipo, acompanhamento, objetivo_alcancado, duracao) 
+VALUES (6, 2, 1, 1, '2026.1', 'Chat Online', 'grupo', 'reativa', 'Dúvidas Arquitetura', 'Conteudo', 'Assincrono', 'Sim', 15);
+
+INSERT INTO public.matricula_intervencao (id_intervencao, id_matricula) 
+VALUES (6, 1), (6, 2);
+
+-- Visualizar Intervenção criada
+SELECT
+    STRING_AGG(e.nome || ' - ' || m.matricula, ', ') AS estudantes,
+    i.data_intervencao as "Data",
+    i.semestre as "Semestre",
+    d.nome as "Disciplina",
+    i.assunto AS "Assunto",
+    i.forma AS "Forma",
+    i.formato AS "Formato",
+    i.interacao AS "Interação",
+    i.tipo AS "Tipo",
+    i.acompanhamento AS "Acompanhamento",
+    i.duracao AS "Duração",
+    i.objetivo_alcancado AS "Objetivo alcançado"
+FROM intervencao i
+    INNER JOIN matricula_intervencao mi ON i.id = mi.id_intervencao
+    INNER JOIN matricula m ON mi.id_matricula = m.id
+    INNER JOIN estudante e ON m.id_estudante = e.id
+    INNER JOIN disciplina d ON i.id_disciplina = d.id
+WHERE i.id = 6;
